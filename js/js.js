1e20d120c4b872d018ae2ff4f1cc33d687628263
yasminGhazy <jesygzaki@gmail.com>
2020-03-27 23:34:44 +0200

    t

--- js/js.js
+++ js/js.js
@@ -31,13 +31,38 @@ window.onclick = function(event) {
  }
 }
 }*/
+window.onclick = function (event) {
+  var view = document.querySelectorAll(".view");
+  var close =document.querySelectorAll(".close");
+  for (var i = 0; i < view.length; i++) {
+    if (view[i] == event.target) {
+      document.getElementById(view[i].dataset.target).style.display = "block"
+      break;
+    }
+  }
+  for (var i = 0; i < close.length; i++) {
+    if (close[i] == event.target) {
+      document.getElementById(close[i].dataset.target).style.display = "none"
+      break;
+    }
+  }
+}
 
-
-
+/*
+var view = document.querySelectorAll(".view");
+for (var i; i < view.length; i++) {
+  if (view[i].dataset.target ==doc) {
+    document.getElementById(view).style.display = "block"
+    break;
+  }
+}
+var view = document.querySelectorAll(".view")[0].dataset.target;
+*/
+/*
 $(".view").on('click',function(event){
 
   $(document.getElementById(event.target.dataset.target)).css({"display":"block"});
 });
 $(".close").on('click',function(event){
   $(document.getElementById(event.target.dataset.target)).css({"display":"none"});
-});
+});*/
