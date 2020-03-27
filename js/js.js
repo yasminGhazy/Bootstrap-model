ab598c48056eac5f96aa26fb293a88ca194b85d1
yasminGhazy <jesygzaki@gmail.com>
2020-03-27 21:52:27 +0200

    n

--- js/js.js
+++ js/js.js
@@ -18,24 +18,11 @@ function closeModalByBtn()
     document.getElementById("modal").style.display = "none";
 
 }*/
-function viewoModal(){
-    
- /* var modal_id=document.getElementsByTagName("button")[0].getAttribute("data-target");
-  document.getElementById(modal_id).style.display = "block";*/
-  var modal_id=event.target.dataset.target;
-  document.getElementById( modal_id).style.display = "block";
-}
-function closeModalByBtn()
-{
-  var modal_id=event.target.dataset.target;
-  /*var modal_id=document.getElementsByTagName("button")[0].getAttribute("data-target");*/
-  document.getElementById( modal_id).style.display = "none";
 
-}
+/*
 
 function closeModalByOverlay(){
-  /*var modal_id=document.getElementsByClassName("close")[0].getAttribute("data-target");
-*/
+
 var modal_id=event.target.dataset.target;
   var modal = document.getElementById(modal_id);
 window.onclick = function(event) {
@@ -43,13 +30,14 @@ window.onclick = function(event) {
   document.getElementById( modal_id).style.display = "none";
  }
 }
-}
+}*/
 
-/*
-$(".view").on('click',function(){
 
-  $(".model_").css({"display":"block"});
+
+$(".view").on('click',function(event){
+
+  $(document.getElementById(event.target.dataset.target)).css({"display":"block"});
 });
-$(".close").on('click',function(){
-  $(".model_").css({"display":"none"});
-});*/
+$(".close").on('click',function(event){
+  $(document.getElementById(event.target.dataset.target)).css({"display":"none"});
+});
