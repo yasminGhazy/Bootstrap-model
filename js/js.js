33080f9d083e0e8205a431b58a0eabe9561effeb
yasminGhazy <jesygzaki@gmail.com>
2020-04-23 00:23:08 +0200

    ES6

--- js/js.js
+++ js/js.js
@@ -1,87 +1,36 @@
 /*
-
-function closeModalByOverlay(){
-
-var modal_id=event.target.dataset.target;
-  var modal = document.getElementById(modal_id);
-window.onclick = function(event) {
- if (event.target == modal) {
-  document.getElementById( modal_id).style.display = "none";
- }
-}
-}*/
-function elem( selector, eventName, fun ){
+function elem(selector, eventName, fun) {
   for (let i = 0; i < document.querySelectorAll(selector).length; i++) {
     document.querySelectorAll(selector)[i].addEventListener(eventName, fun);
-   }
+  }
 }
 
- elem('.view', 'click', function (e) {
+elem('.view', 'click', function (e) {
   document.getElementById(this.dataset.target).style.display = "block";
+
 });
 
 elem('.close', 'click', function (e) {
   document.getElementById(this.dataset.target).style.display = "none";
 });
-/*
-for (let i = 0; i < document.querySelectorAll(".view").length; i++) {
- let value =document.querySelectorAll(".view")[i].dataset.target;
-  document.querySelectorAll(".view")[i].addEventListener("click", function() {
-    viewModel(value);
-  });
-}
-
-for (let i = 0; i < document.querySelectorAll(".close").length; i++) {
-  let value =document.querySelectorAll(".close")[i].dataset.target;
-  document.querySelectorAll(".close")[i].addEventListener("click", function(){
-    closeModel(value);
-  });
-
-}
-function viewModel(v) {
-  document.getElementById(v).style.display = "block";
-
-}
-function closeModel(v) {
-  document.getElementById(v).style.display = "none";
-
-}
+  
 */
-/*
-window.onclick = function (event) {
-  var view = document.querySelectorAll(".view");
-  var close =document.querySelectorAll(".close");
-  for (var i = 0; i < view.length; i++) {
 
-    if (view[i] == event.target) {
-      document.getElementById(view[i].dataset.target).style.display = "block"
-
-      break;
+class modal {
+run() {
+    let viewSelector =document.querySelectorAll('.view');
+    for (let i = 0; i < viewSelector.length; i++) {
+      viewSelector[i].addEventListener('click', function () {
+        document.getElementById(this.dataset.target).style.display = "block";
+      });
+    }
+    let closeSelector =document.querySelectorAll('.close');
+    for (let i = 0; i < closeSelector.length; i++) {
+      closeSelector[i].addEventListener('click', function () {
+        document.getElementById(this.dataset.target).style.display = "none";
+      });
     }
   }
-  for (var i = 0; i < close.length; i++) {
-    if (close[i] == event.target) {
-      document.getElementById(close[i].dataset.target).style.display = "none"
-      break;
-    }
-  }
-}*/
-
-/*
-var view = document.querySelectorAll(".view");
-for (var i; i < view.length; i++) {
-  if (view[i].dataset.target ==doc) {
-    document.getElementById(view).style.display = "block"
-    break;
-  }
 }
-var view = document.querySelectorAll(".view")[0].dataset.target;
-*/
-/*
-$(".view").on('click',function(event){
-
-  $(document.getElementById(event.target.dataset.target)).css({"display":"block"});
-});
-$(".close").on('click',function(event){
-  $(document.getElementById(event.target.dataset.target)).css({"display":"none"});
-});*/
+let myModal = new modal();
+myModal.run();
